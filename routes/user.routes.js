1b3ddb8bcffa3a4f906f08d7eaaf94262cf6a653
jaythale <jaythale@123.com>
2024-07-26 15:21:15 +0530

    result key update

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,5 +1,7 @@
 const router = require("express").Router()
+const { userProtected } = require("../middleware/protected")
 const userController = require("./../controllers/user.controller")
 router
-    .post("/Verify-user-email", userController, userController.verifyUserEmail)
+
+    .post("/Verify-user-email", userProtected, userController.verifyUserEmail)
 module.exports = router
