4c693f0a838ba16d3bdb51655140bad272e6c163
jaythale <jaythale@123.com>
2024-07-27 15:21:21 +0530

    route update

--- middleware/protected.js
+++ middleware/protected.js
@@ -9,6 +9,7 @@ exports.userProtected = (req, res, next) => {
     jwt.verify(user, process.env.JWT_KEY, (err, decode) => {
         console.log(decode)
         if (err) {
+            console.log(err)
             return res.status(401).json({ message: "JWT Error", error: err.message })
         }
         req.loggedInUser = decode.userId
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -8,7 +8,7 @@ router
 
     .post("/login-mobile-user", authController.loginUser)
     .post("/register-mobile-user", authController.registerUser)
-    .post("/logout-mobile-user", authController.loginUser)
+    .post("/logout-mobile-user", authController.logoutUser)
 
 
 module.exports = router
