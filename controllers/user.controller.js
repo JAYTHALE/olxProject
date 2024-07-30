8a426086d96e98e2ebb8959e45ed7864c5f3691e
jaythale <jaythale@123.com>
2024-07-30 14:43:04 +0530

    user post added

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -77,11 +77,11 @@ exports.verifyMobileOTP = asyncHandler(async (req, res) => {
 })
 
 exports.addPost = asyncHandler(async (req, res) => {
-    const { title, desc, price, images, location } = req.body
-    const { error, isError } = checkEmpty({ title, desc, price, images, location })
+    const { title, desc, price, images, location, category } = req.body
+    const { error, isError } = checkEmpty({ title, desc, price, images, location, category })
     if (isError) {
         return res.status(400).json({ message: "All Fields Required", error })
     }
-    await Posts.create({ title, desc, price, images, location, user: req.loggedInUser })
+    await Posts.create({ title, desc, price, images, location, category, user: req.loggedInUser })
     res.json({ message: "Post create Success" })
 })
